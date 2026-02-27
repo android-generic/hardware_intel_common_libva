acd63290cb67fa5051845d43ec84f7fdf54b174e
hmtheboy154 <buingoc67@gmail.com>
2026-02-27 07:26:53 -0500

    Revert "Remove unnecessary Android code"
    This reverts commit 3da1ba7e3cac635c6dc4d5d4cd1234d386926b49.

--- va/va_android.h
+++ va/va_android.h
@@ -31,4 +31,19 @@
 /** \brief Android ION buffer memory type. */
 #define VA_SURFACE_ATTRIB_MEM_TYPE_ANDROID_ION      0x00200000
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/*
+ * Returns a suitable VADisplay for VA API
+ */
+VADisplay vaGetDisplay(
+    void *android_dpy
+);
+
+#ifdef __cplusplus
+}
+#endif
+
 #endif /* _VA_ANDROID_H_ */
